4591cca110c8e89aef3ed4e8602a37ca2831124d
Marco Godinho <aurelio.marco.ag@gmail.com>
2018-02-20 19:13:34 -0300

    Added entrypoint

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,7 @@ RUN usermod www-data -u 82; groupmod -g 82 www-data;
 
 #Add deploy user with UID 2500
 RUN useradd deploy -u 2500 -g www-data -s /bin/bash
+
+ADD ./entrypoint.sh /entrypoint.sh
+RUN chmod u+x /entrypoint.sh
+ENTRYPOINT ["sh", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if  grep -r "varnish:6081" /etc/nginx ;then
+	echo "Looking for 'varnish'..." >> /var/log/nginx/startup.log
+	while ! ping -c1 varnish 1>/dev/null 2>/dev/null ;do
+		echo "Unable to resolve 'varnish'. Trying again in 5s" >> /var/log/nginx/startup.log
+		sleep 5
+	done
+	echo "Found 'varnish'. Starting Nginx." >> /var/log/nginx/startup.log
+fi
+
+exec /usr/sbin/nginx -g daemon off
