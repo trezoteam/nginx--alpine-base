6d2e4596df0c0c87ecce37378ddfc56371f5f07f
Marco Aurélio Alano Godinho <marco@trezo.com.br>
2018-02-22 16:03:07 -0300

    Get varnish ip from /etc/opt/nginx_hosts

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-echo "127.0.0.1 nginx" >> /etc/hosts && echo "127.0.0.1 varnish" >> /etc/hosts
+echo "127.0.0.1 nginx" >> /etc/hosts
+
+if [ -f /etc/opt/nginx_hosts ];then
+  cat /etc/opt/nginx_hosts >> /etc/hosts
+else
+  echo "127.0.0.1 varnish" >> /etc/hosts 
+fi
+
 sed -i 's/^hosts:.*/hosts: dns files/g' /etc/nsswitch.conf
 exec /usr/sbin/nginx -g "daemon off;"
