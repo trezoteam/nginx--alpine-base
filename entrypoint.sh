e54477dea6802147cbe850ede715146eb8e52ec4
Marco Aurélio Alano Godinho <marco@trezo.com.br>
2018-02-22 17:43:22 -0300

    Entrypoint to wait for hosts file when WAIT_VARNISH is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 echo "127.0.0.1 nginx" >> /etc/hosts
-sleep 5
-if [ -f /etc/opt/nginx/hosts ];then
+>/var/log/nginx/startup.log
+
+if [ -n "$WAIT_VARNISH" ];then
+  while [ ! -f /etc/opt/nginx/hosts ];do
+    echo "looking for /etc/opt/nginx/hosts..." >> /var/log/nginx/startup.log
+    sleep 1
+  done
   cat /etc/opt/nginx/hosts >> /etc/hosts
-else
-  echo "127.0.0.1 varnish" >> /etc/hosts 
 fi
 
 sed -i 's/^hosts:.*/hosts: dns files/g' /etc/nsswitch.conf
